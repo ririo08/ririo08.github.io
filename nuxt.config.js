80fcefb862db68871dfc0fea77f4bf10fbedfaf0
ririo08 <ririomail@gmail.com>
2021-08-03 15:13:08 +0900

    ⚙Sass Loaderを追加

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,9 +4,9 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'Profile',
+    title: 'リリオのプロフィール',
     htmlAttrs: {
-      lang: 'en'
+      lang: 'ja'
     },
     meta: [
       { charset: 'utf-8' },
