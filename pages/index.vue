0bf230ac0790d65161837306e14a08459cd5211f
ririo08 <ririomail@gmail.com>
2021-08-03 17:50:32 +0900

    ⚙制作物を追加

--- pages/index.vue
+++ pages/index.vue
@@ -12,8 +12,148 @@
       <h2 class="header-h2">ririo08</h2>
     </header>
     <main>
-      リリオさんの自己紹介をしましょう。
-      <p class="main-content">ゲーム実況者兼ストリーマーです。</p>
+      <div class="main">
+        <p class="main-content">リリオさんの自己紹介をしましょう。</p>
+        <p class="main-content">ゲーム実況者兼ストリーマーです。</p>
+        <h3>映像コンテンツ等</h3>
+        <ul class="listBox">
+          <li class="listBox-container">
+            <div class="listBox-container_img">
+              <img src="img/icon/youtube.png" alt="" />
+            </div>
+            <div class="listBox-container_text">
+              <a
+                class="link"
+                href="https://youtube.com/RirioTV"
+                target="_blank"
+                rel="noopener noreferrer"
+                >YouTubeチャンネル「リリオ」</a
+              >
+              <p class="url">https://youtube.com/RirioTV</p>
+              <p class="detail">
+                YouTuberとして動画を投稿しているチャンネル。主にゲーム実況を上げています。
+              </p>
+            </div>
+          </li>
+          <li class="listBox-container">
+            <div class="listBox-container_img">
+              <img src="img/icon/youtube.png" alt="" />
+            </div>
+            <div class="listBox-container_text">
+              <a
+                class="link"
+                href="https://www.youtube.com/channel/UCYYTax3608_cd88AY2oq39Q"
+                target="_blank"
+                rel="noopener noreferrer"
+                >YouTubeチャンネル「リリオの日常」</a
+              >
+              <p class="url">
+                https://www.youtube.com/channel/UCYYTax3608_cd88AY2oq39Q
+              </p>
+              <p class="detail">
+                シンプルにサブチャンネルです。低クオリティな動画が多め。
+              </p>
+            </div>
+          </li>
+          <li class="listBox-container">
+            <div class="listBox-container_img">
+              <img src="img/icon/youtube.png" alt="" />
+            </div>
+            <div class="listBox-container_text">
+              <a
+                class="link"
+                href="https://www.youtube.com/channel/UCyRnuuwAPPLaGLf_xGykMPA"
+                target="_blank"
+                rel="noopener noreferrer"
+                >YouTubeチャンネル「リリオの配信局」</a
+              >
+              <p class="url">
+                https://www.youtube.com/channel/UCyRnuuwAPPLaGLf_xGykMPA
+              </p>
+              <p class="detail">
+                基本的に生放送などの配信コンテンツはこちらで発信しています。ほぼ毎日やってます。
+              </p>
+            </div>
+          </li>
+        </ul>
+        <h3>Webサイト</h3>
+        <ul class="listBox">
+          <li class="listBox-container">
+            <div class="listBox-container_img">
+              <img src="img/icon/homepage.png" alt="" />
+            </div>
+            <div class="listBox-container_text">
+              <a
+                class="link"
+                href="https://ririotv.xyz/"
+                target="_blank"
+                rel="noopener noreferrer"
+                >リリオのホームページ</a
+              >
+              <p class="url">https://ririotv.xyz/</p>
+              <p class="detail">
+                自分のホームページです。NuxtをStaticでGithub
+                PagesにDeployして公開しています。（ルー語）
+              </p>
+            </div>
+          </li>
+          <li class="listBox-container">
+            <div class="listBox-container_img">
+              <img src="img/icon/blog.png" alt="" />
+            </div>
+            <div class="listBox-container_text">
+              <a
+                class="link"
+                href="https://ririogame.hatenablog.com//"
+                target="_blank"
+                rel="noopener noreferrer"
+                >リリオの日記型倉庫3</a
+              >
+              <p class="url">https://ririogame.hatenablog.com/</p>
+              <p class="detail">
+                はてなブログです。YouTubeを始めてから更新頻度は低め。
+              </p>
+            </div>
+          </li>
+        </ul>
+        <h3>Web App</h3>
+        <ul class="listBox">
+          <li class="listBox-container">
+            <div class="listBox-container_img">
+              <img src="img/icon/nightcode.png" alt="" />
+            </div>
+            <div class="listBox-container_text">
+              <a
+                class="link"
+                href="https://ririo08.github.io/25-Night-Code/"
+                target="_blank"
+                rel="noopener noreferrer"
+                >ナイトコードの作業を覗きに行くツール</a
+              >
+              <p class="url">https://ririo08.github.io/25-Night-Code/</p>
+              <p class="detail">
+                プロセカの24時間の作業BGMを実時間で再生できるツール
+              </p>
+            </div>
+          </li>
+          <li class="listBox-container">
+            <div class="listBox-container_img">
+              <img src="img/icon/javascript.png" alt="" />
+            </div>
+            <div class="listBox-container_text">
+              <a
+                class="link"
+                href="https://ririo08.github.io/Ririos-Script/"
+                target="_blank"
+                rel="noopener noreferrer"
+                >リリオのスクリプト置き場</a
+              >
+              <p class="url">https://ririo08.github.io/Ririos-Script/</p>
+              <p class="detail">ブックマークレット類が置いてあります。</p>
+            </div>
+          </li>
+        </ul>
+      </div>
     </main>
   </div>
 </template>
@@ -23,18 +163,24 @@ export default {};
 </script>
 
 <style lang="scss">
+* {
+  box-sizing: border-box;
+}
+
 body {
   margin: auto;
   background-color: #faf1eb;
 }
 .container {
-  max-inline-size: 600px;
+  max-inline-size: 100%;
   inline-size: 100%;
   margin: auto;
+  padding-left: 20px;
 }
 
 .header {
   margin-block-start: 100px;
+  padding-right: 20px;
   &-imgbox {
     max-inline-size: 200px;
     inline-size: 80%;
@@ -58,11 +204,63 @@ body {
   }
 }
 
-main {
+.main {
   margin-block-start: 50px;
+  inline-size: 100%;
+  &-content {
+    line-height: 1.7em;
+    inline-size: 100%;
+  }
 }
 
-.main-content {
-  line-height: 1.7em;
+.listBox {
+  list-style-type: none;
+  padding-left: 0;
+  &-container {
+    display: flex;
+    flex: 1;
+    &_img {
+      width: 64px;
+      margin-top: 5px;
+      & img {
+        width: 64px;
+        border: 1px #ccc solid;
+        border-radius: 5px;
+      }
+    }
+    &_text {
+      width: 100%;
+      margin-left: 10px;
+    }
+  }
+}
+
+.link {
+  font-size: 1.6rem;
+  color: #ff9600;
+}
+
+.url {
+  width: 100%;
+  margin-top: 0;
+  margin-bottom: 0;
+  font-size: 0.8rem;
+  color: #555;
+  word-break: break-all;
+}
+
+.detail {
+  font-size: 1rem;
+  margin-top: 0;
+}
+
+@media screen and (max-width:767px) {
+  .container {
+    padding-left: 5px;
+    padding-right: 5px;
+  }
+  .header {
+    padding-right: 0;
+  }
 }
 </style>
