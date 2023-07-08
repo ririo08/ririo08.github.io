6208da6e7c241fa33403aabb530d5f69862f402f
ririo08 <ririomail@gmail.com>
2023-07-08 19:08:16 +0900

    wip: 静的生成されないので一旦戻す

--- pages/index.vue
+++ pages/index.vue
@@ -6,12 +6,7 @@ import { links } from "assets/links"
   <div class="container">
     <header class="header">
       <div class="header-imgbox">
-        <nuxt-img
-          format="webp"
-          class="header-imgbox_content"
-          src="ririo.png"
-          alt="犬のアイコン"
-        />
+        <img class="header-imgbox_content" src="ririo.png" alt="犬のアイコン" />
       </div>
       <h1 class="header-h1">リリオ</h1>
       <h2 class="header-h2">ririo08</h2>
@@ -25,7 +20,7 @@ import { links } from "assets/links"
           <ul class="listBox">
             <li v-for="profile of profiles.contents" class="listBox-container">
               <div class="listBox-container_img">
-                <nuxt-img format="webp" :src="profile.imageUrl" alt="" />
+                <img :src="profile.imageUrl" alt="" />
               </div>
               <div class="listBox-container_text">
                 <a
